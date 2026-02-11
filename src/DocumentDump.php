48672cbcfc9d9fe388a90fc2e024962021d08df5
Toby Allen <toby@cookingisfun.ie>
2026-02-11 21:22:30 +0000

    output

--- src/DocumentDump.php
+++ src/DocumentDump.php
@@ -3,6 +3,7 @@
 namespace Tobya\Documentdump;
 
 use Tobya\Docto\CLITool;
+use Illuminate\Support\Facades\Process;
 
 class Documentdump extends CLITool {
 
@@ -11,11 +12,14 @@ class Documentdump extends CLITool {
     public function run() {
         // bcsdumprec.exe -O c:\BCSSERVER\RecipeDiff\Recipes_Doc\ --DAYS 5  -M -N  -S false
         $cmdline = $this->build_commandline();
+        $result = Process::command( $cmdline)->run();
         echo $cmdline;
+        echo $result->output();
 
     }
 
-    public function output($value){
+    public function output($value):Documentdump
+    {
         return $this->Parameter('-O',$value);
     }
 
